a56ebb408dd9384efcbaaa847bcd9cccdd9fee38
varun <varunsureka123@gmail.com>
2019-10-11 10:14:04 -0500

    Commited

--- src/Challenge3.java
+++ src/Challenge3.java
@@ -3,8 +3,15 @@ Varun Sureka
 10/4/19
 Java Period 2
 */
+import sun.jvm.hotspot.memory.Space;
 
-public class Challenge3 {
+import java.util.Scanner;
+public class Challenge3
+{
+    //Scanner scaleScanner = new Scanner(System.in);
+    //final int x = scaleScanner.nextInt();
+    final static int x = 3;
+
     public static void main(String[] args)
     {
         PlusLine();
@@ -19,36 +26,28 @@ public class Challenge3 {
     public static void PlusLine()
     {
         System.out.print("+");
-        for (int x =9; x>0; x--)
+        for (int y =0; y<2*x+2; y++)
         {
-            System.out.print("-");
+            if (y == 2*x+1)
+            {
+                System.out.print("+");
+            }
+            else
+            {
+                System.out.print("-");
+            }
         }
-        System.out.print("+");
         System.out.print('\n');
     }
 
     public static void TopHalfTop()
     {
-        for (int line = 4; line>0;line--)
+        for (int line = x; line>0;line--)
         {
             System.out.print("|");
-            for (int space1 = 0; space1<(line); space1++)
-            {
-                System.out.print(" ");
-            }
-            for (int slash = 0; slash<(4-line); slash++)
-            {
-                System.out.print("/");
-            }
-            System.out.print("*");
-            for (int slash = 0; slash<(4-line); slash++)
-            {
-                System.out.print("\\");
-            }
-            for (int space1 = 0; space1<(line); space1++)
-            {
-                System.out.print(" ");
-            }
+            Spaces(line);
+            Pyramid(line);
+            Spaces(line);
             System.out.print("|");
             System.out.print('\n');
         }
@@ -56,28 +55,56 @@ public class Challenge3 {
 
     public static void TopHalfBottom()
     {
-        for(int line = 4; line>0; line--)
+        for(int line = x; line>0; line--)
         {
             System.out.print("|");
-            for (int space = 0; space <(5-line); space++)
-            {
-                System.out.print(" ");
-            }
-            for (int slash = 1; slash<line; slash++)
-            {
-                System.out.print("\\");
-            }
-            System.out.print("*");
-            for (int slash = 1; slash<line; slash++)
-            {
-                System.out.print("/");
-            }
-            for (int space = 0; space <(5-line); space++)
-            {
-                System.out.print(" ");
-            }
+            SpaceDown(line);
+            PyramidDown(line);
+            SpaceDown(line);
             System.out.print("|");
             System.out.print('\n');
+        }
+    }
+
+    public static void Spaces(int line)
+    {
+        for (int space = 0; space<(line); space++)
+        {
+            System.out.print(" ");
+        }
+    }
+
+    public static void Pyramid(int line)
+    {
+        for (int slash = 0; slash<(x-line); slash++)
+        {
+            System.out.print("/");
+        }
+        System.out.print("*");
+        for (int slash = 0; slash<(x-line); slash++)
+        {
+            System.out.print("\\");
+        }
+    }
+
+    public static void SpaceDown(int line)
+    {
+        for (int space =0; space <((x+1)-line); space++)
+        {
+            System.out.print(" ");
+        }
+    }
+
+    public static void PyramidDown(int line)
+    {
+        for (int slash = 1; slash<line; slash++)
+        {
+            System.out.print("\\");
+        }
+        System.out.print("*");
+        for (int slash = 1; slash<line; slash++)
+        {
+            System.out.print("/");
         }
     }
 }
